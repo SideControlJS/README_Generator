712d4703ed4dfab007cdbb24286c1d1bfbb5e13b
Jesse <jesse.lare@gmail.com>
2023-09-10 13:46:04 -0400

    updating license return

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,7 @@
 //function that returns license badge
 function renderBadge(license) {
     if (license !== "none") {
-        return `![Github license](https://img.shields.io/badge/license${license}-blue.svg)`;
+        return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`;
     }
     return "";
 }
